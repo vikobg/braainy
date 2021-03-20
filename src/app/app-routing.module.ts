1b581451136fbb7673373cff587c385175c81363
Viktor Borisov <viktor.borisov@mail.schwarz>
2021-03-20 14:53:12 +0200

    Add basic modules - core, shared, product, contacts and basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes =  [
+  {path: '', redirectTo: 'products', pathMatch: 'full'},
+  {path: 'products', loadChildren: () => import('./modules/product/product.module').then(m => m.ProductModule)},
+  {path: 'contacts', loadChildren: () => import('./modules/contacts/contacts.module').then(m => m.ContactsModule)},
+  {path: '**', redirectTo: 'products'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
