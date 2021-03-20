61d9cb2cc1391e7069a522264cf5d0e96e2c7beb
Viktor Borisov <viktor.borisov@mail.schwarz>
2021-03-20 14:57:44 +0200

    Add very simple material ui navigation for products and contacts

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatSidenavModule,
+    MatListModule,
+  ],
+  exports: [MatSidenavModule, MatListModule]
 })
 export class SharedModule { }
